ec244b1250f3331cde24d9e346ab1935b956e5b2
Daniel O'Driscoll <daodrisc@tcd.ie>
2022-05-07 22:20:02 +0100

    blank changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :travel_logs
-root 'travel_logs#index'
+  root 'travel_logs#index'
 
   get "sign_up", to: 'registrations#new'
   post "sign_up", to: 'registrations#create'
