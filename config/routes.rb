72440353d11aefb6c2825d89a12d5f59954eecdb
Daniel O'Driscoll <daodrisc@tcd.ie>
2022-05-07 20:46:17 +0100

    added deployment pipeline

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
 root 'travel_logs#index'
 
   get "sign_up", to: 'registrations#new'
+  post "sign_up", to: 'registrations#create'
 
   get '/journey_calculator' => 'distfind#journeyCalculator'
 
   post '/distance_profile' => 'distfind#distanceProfileCalc'
+  
 end
---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env #!/usr/bin/env bash
+#Check if there is instance running with the image name we are deploying
+
+CURRENT_INSTANCE=$(sudo docker ps -a -q --filter ancestor="$IMAGE_NAME" --format="{{.ID}}")
+
+#if an instance does exist stop the CURRENT_INSTANCE
+if [ "$CURRENT_INSTANCE" ]
+then
+  sudo docker rm $(sudo docker stop $CURRENT_INSTANCE)
+fi
+
+# Pull down the isntance from dockerhub
+sudo docker pull $IMAGE_NAME
+
+#Check if docker container exists with the name of $CONTAINER_NAME if it does remove the CONTAINER_NAME
+CONTAINER_EXISTS=$(sudo docker ps -a | grep $CONTAINER_NAME)
+if [ "$CONTAINER_EXISTS" ]
+then
+  sudo docker rm $CONTAINER_NAME
+fi
+
+sudo docker run -p 3000:3000 -d --name $CONTAINER_NAME $IMAGE_NAME
